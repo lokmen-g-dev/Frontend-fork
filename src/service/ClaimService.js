e2bc896b8eadcddc55f2d9509cf1011e461ebbf3
Wassim BESSIOUD <72418368+Wassim-Bessioud@users.noreply.github.com>
2023-12-11 13:15:30 +0100

    Update ClaimService.js

--- src/service/ClaimService.js
+++ src/service/ClaimService.js
@@ -4,57 +4,57 @@ class ClaimService {
 
 
     createClaim = (claim,studentId) => {
-        return axios.post("http://esprit.back-end.tn:8085/reclamations/" + studentId , claim)
+        return axios.post("http://esprit.reclamation.tn:8085/reclamations/" + studentId , claim)
     }
 
     getClaimsByStudentId = (studentId) => {
-        return axios.get("http://esprit.back-end.tn:8085/reclamations/" + studentId )
+        return axios.get("http://esprit.reclamation.tn:8085/reclamations/" + studentId )
     }
 
     deleteClaim =(claimId)=> {
-        return axios.delete("http://esprit.back-end.tn:8085/reclamations/" + claimId)
+        return axios.delete("http://esprit.reclamation.tn:8085/reclamations/" + claimId)
     }
 
     updateClaim =(claim,claimId,StudentId) => {
-        return axios.put("http://esprit.back-end.tn:8085/reclamations/" + claimId + '/' + StudentId , claim);
+        return axios.put("http://esprit.reclamation.tn:8085/reclamations/" + claimId + '/' + StudentId , claim);
     }
     
     getAllClaims = () => {
-        return axios.get("http://esprit.back-end.tn:8085/reclamations");
+        return axios.get("http://esprit.reclamation.tn:8085/reclamations");
     }
 
     getClaimsInfo = () => {
-        return axios.get("http://esprit.back-end.tn:8085/anneeuniv");
+        return axios.get("http://esprit.reclamation.tn:8085/anneeuniv");
     }
 
     updateClaiStatu = (claim,claimId) => {
-        return axios.put("http://esprit.back-end.tn:8085/reclamations/status/" + claimId , claim)
+        return axios.put("http://esprit.reclamation.tn:8085/reclamations/status/" + claimId , claim)
     }
 
     getClaimByTime = (dateRec) => {
-        return axios.get("http://esprit.back-end.tn:8085/reclamations/date/" + dateRec)
+        return axios.get("http://esprit.reclamation.tn:8085/reclamations/date/" + dateRec)
     }
     sendClaimToEmployee = (claimId,employeeId) => {
-        return axios.put("http://esprit.back-end.tn:8085/reclamations/employee/" + employeeId + "/" + claimId)
+        return axios.put("http://esprit.reclamation.tn:8085/reclamations/employee/" + employeeId + "/" + claimId)
     }
 
     getClaimsByEmployeeId = (employeeId) => {
-        return axios.get("http://esprit.back-end.tn:8085/reclamations/employee/" + employeeId )
+        return axios.get("http://esprit.reclamation.tn:8085/reclamations/employee/" + employeeId )
     }
 
     getClaimsStdInfo = (claimId) => {
-        return axios.get("http://esprit.back-end.tn:8085/reclamation/infos/" + claimId);
+        return axios.get("http://esprit.reclamation.tn:8085/reclamation/infos/" + claimId);
     }
     getClaimsEmpInfo = (claimId) => {
-        return axios.get("http://esprit.back-end.tn:8085/reclamation/employee/infos/" + claimId);
+        return axios.get("http://esprit.reclamation.tn:8085/reclamation/employee/infos/" + claimId);
     }
 
     sendResponse = (resp,claimId) => {
-        return axios.post("http://esprit.back-end.tn:8085/response/new/" + claimId , resp)
+        return axios.post("http://esprit.reclamation.tn:8085/response/new/" + claimId , resp)
     }
 
     getResponse = (claimId) => {
-        return axios.get("http://esprit.back-end.tn:8085/response/" + claimId);
+        return axios.get("http://esprit.reclamation.tn:8085/response/" + claimId);
     }
 
 
