8c466a7145342f7d9b77c171143edbf587d28a11
Wassim BESSIOUD <72418368+Wassim-Bessioud@users.noreply.github.com>
2023-12-11 12:53:09 +0100

    Update NotifService.js

--- src/service/NotifService.js
+++ src/service/NotifService.js
@@ -3,29 +3,29 @@ import axios from "axios"
 
 class NotifService {
     sendNotifToAdmin = (notif) => {
-        return axios.post("http://esprit.reclamation.tn:8085/Notification/admin",notif)
+        return axios.post("http://esprit.back-end.tn:8085/Notification/admin",notif)
     }
 
    
     sendNotifsStudent = (notif,studentId) => {
-        return axios.post("http://esprit.reclamation.tn:8085/notifications/students/" + studentId , notif)
+        return axios.post("http://esprit.back-end.tn:8085/notifications/students/" + studentId , notif)
     }
     sendNotifsEmployee = (notif,employeeId) => {
-        return axios.post("http://esprit.reclamation.tn:8085/notifications/employees/" + employeeId , notif)
+        return axios.post("http://esprit.back-end.tn:8085/notifications/employees/" + employeeId , notif)
     }
     sendNotifsAdmin = (notif) => {
-        return axios.post("http://esprit.reclamation.tn:8085/Notification/admin/" , notif)
+        return axios.post("http://esprit.back-end.tn:8085/Notification/admin/" , notif)
     }
 
     deleteNotifById = (notifId) => {
-        return axios.delete("http://esprit.reclamation.tn:8085/notifs/"+ notifId)
+        return axios.delete("http://esprit.back-end.tn:8085/notifs/"+ notifId)
     }
 
     deleteEmployeeNotifs = (employeeId) => {
-        return axios.delete("http://esprit.reclamation.tn:8085/employees/"+employeeId+"/notificaitons")
+        return axios.delete("http://esprit.back-end.tn:8085/employees/"+employeeId+"/notificaitons")
     }
     deleteStudentsNotifs = (studentId) => {
-        return axios.delete("http://esprit.reclamation.tn:8085/students/"+studentId+"/notificaitons")
+        return axios.delete("http://esprit.back-end.tn:8085/students/"+studentId+"/notificaitons")
     }
 } 
 
